697dfb4749a5e2d86a2403e102a18b86c9c4e649
Ngqabutho-Moyo <your.moyongqaa@gmail.com>
2025-09-05 16:30:24 +0200

    Enabled CORS

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,7 @@ import {
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   const config = new DocumentBuilder()
     .addBasicAuth()
     .setTitle('Mukai API Docs')
